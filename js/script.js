f6a168130aee763634a3c1a12616623e19be7f0f
Chirkov Slava <slavachirkov92@gmail.com>
2023-03-24 20:33:40 +0300

    completed task26hard

--- js/script.js
+++ js/script.js
@@ -4,14 +4,22 @@ const getData = (url) => {
 	return fetch(url).then(response => response.json())
 }
 
-const sendData = (url, data) => {
-	return fetch(url, {
-		method: 'POST',
-		body: data,
-		headers: {
-			'Content-type': 'application/json; chrset=UTF-8',
-		},
-	}).then(response => response.json())
-}
+// const sendData = (url, data) => {
+// 	return fetch(url, {
+// 		method: 'POST',
+// 		body: data,
+// 		headers: {
+// 			'Content-type': 'application/json; chrset=UTF-8',
+// 		},
+// 	}).then(response => response.json())
+// }
 
-getData('db.json').then(data => sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(data)))
+// getData('db.json').then(data => sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(data)))
+
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts')
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+getData('db.json').then(data => xhr.send(JSON.stringify(data)))
